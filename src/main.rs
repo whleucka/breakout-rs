b5eae8a3fa295c3112656399682df819f48f5583
William Hleucka <william.hleucka@gmail.com>
2023-05-13 19:39:38 -0600

    feat: ball movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,52 @@
 use macroquad::prelude::*;
 
+struct Ball {
+    active: bool,
+    x: f32,
+    y: f32,
+    dx: i8,
+    dy: i8,
+    r: f32,
+    color: Color,
+    speed: f32,
+}
+
+impl Ball {
+    pub fn new() -> Self {
+        Self {
+            active: true,
+            x: 150.0,
+            y: 50.0,
+            r: 5.0,
+            dx: 1,
+            dy: -1,
+            color: WHITE,
+            speed: 5.0,
+        }
+    }
+    pub fn movement(&mut self) {
+        if self.x - self.r < 0.0 {
+            self.dx = 1;
+        } else if self.x + self.r > screen_width() {
+            self.dx = -1;
+        }
+        if self.y - self.r < 0.0 {
+            self.dy = 1;
+        } else if self.y + self.r > screen_height() {
+            self.dy = -1;
+            // Ball is dead
+            self.active = false;
+        }
+        self.x += self.speed * self.dx as f32;
+        self.y += self.speed * self.dy as f32;
+    }
+    pub fn draw(&mut self) {
+        if self.active {
+            draw_circle(self.x, self.y, self.r, self.color);
+        }
+    }
+}
+
 struct Player {
     x: f32,
     y: f32,
@@ -7,10 +54,10 @@ struct Player {
     h: f32,
     speed: f32,
     color: Color,
-    direction: Direction,
+    direction: PlayerDirection,
 }
 
-enum Direction {
+enum PlayerDirection {
     IDLE,
     LEFT,
     RIGHT,
@@ -27,7 +74,7 @@ impl Player {
             h,
             speed: 15.0,
             color: RED,
-            direction: Direction::IDLE,
+            direction: PlayerDirection::IDLE,
         }
     }
     pub fn left(&mut self) {
@@ -47,9 +94,9 @@ impl Player {
     pub fn idle(&mut self) {}
     pub fn movement(&mut self) {
         match self.direction {
-            Direction::IDLE => self.idle(),
-            Direction::LEFT => self.left(),
-            Direction::RIGHT => self.right(),
+            PlayerDirection::IDLE => self.idle(),
+            PlayerDirection::LEFT => self.left(),
+            PlayerDirection::RIGHT => self.right(),
         }
     }
     pub fn draw(&mut self) {
@@ -58,17 +105,18 @@ impl Player {
 }
 
 struct Game {
+    ball: Ball,
     player: Player,
 }
 
 impl Game {
     pub fn event_handler(&mut self) {
         if is_key_down(KeyCode::H) || is_key_down(KeyCode::Left) {
-            self.player.direction = Direction::LEFT;
+            self.player.direction = PlayerDirection::LEFT;
         } else if is_key_down(KeyCode::L) || is_key_down(KeyCode::Right) {
-            self.player.direction = Direction::RIGHT;
+            self.player.direction = PlayerDirection::RIGHT;
         } else {
-            self.player.direction = Direction::IDLE;
+            self.player.direction = PlayerDirection::IDLE;
         }
     }
 }
@@ -86,13 +134,16 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut game = Game {
+        ball: Ball::new(),
         player: Player::new(),
     };
     loop {
         clear_background(BLACK);
         game.event_handler();
         game.player.movement();
+        game.ball.movement();
         game.player.draw();
+        game.ball.draw();
         next_frame().await
     }
 }
