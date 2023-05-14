c5a1bceb7594e78729e44979392a05880c728901
William Hleucka <william.hleucka@gmail.com>
2023-05-13 18:40:31 -0600

    feat: player movement

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,15 @@ struct Player {
     y: f32,
     w: f32,
     h: f32,
+    speed: f32,
     color: Color,
+    direction: Direction,
+}
+
+enum Direction {
+    IDLE,
+    LEFT,
+    RIGHT,
 }
 
 impl Player {
@@ -17,7 +25,31 @@ impl Player {
             y: screen_height() - h - 100.0,
             w,
             h,
+            speed: 15.0,
             color: RED,
+            direction: Direction::IDLE,
+        }
+    }
+    pub fn left(&mut self) {
+        if self.x - self.speed < 0.0 {
+            self.x = 0.0;
+        } else {
+            self.x = self.x - self.speed;
+        }
+    }
+    pub fn right(&mut self) {
+        if self.x + self.w + self.speed > screen_width() {
+            self.x = screen_width() - self.w;
+        } else {
+            self.x = self.x + self.speed;
+        }
+    }
+    pub fn idle(&mut self) {}
+    pub fn movement(&mut self) {
+        match self.direction {
+            Direction::IDLE => self.idle(),
+            Direction::LEFT => self.left(),
+            Direction::RIGHT => self.right(),
         }
     }
     pub fn draw(&mut self) {
@@ -27,6 +59,18 @@ impl Player {
 
 struct Game {
     player: Player,
+}
+
+impl Game {
+    pub fn event_handler(&mut self) {
+        if is_key_down(KeyCode::H) || is_key_down(KeyCode::Left) {
+            self.player.direction = Direction::LEFT;
+        } else if is_key_down(KeyCode::L) || is_key_down(KeyCode::Right) {
+            self.player.direction = Direction::RIGHT;
+        } else {
+            self.player.direction = Direction::IDLE;
+        }
+    }
 }
 
 fn window_conf() -> Conf {
@@ -46,6 +90,8 @@ async fn main() {
     };
     loop {
         clear_background(BLACK);
+        game.event_handler();
+        game.player.movement();
         game.player.draw();
         next_frame().await
     }
